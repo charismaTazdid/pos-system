58e241750c7e6085210f423ba214148a591f138b
raihan tajdid <raihantajdid@gmail.com>
2023-06-09 06:33:12 +0600

    simple pos system done

---
+++ README.md
@@ -0,0 +1,12 @@
+## Simple POS system using react.js
+
+<p>Goal: explore react_to_pdf. </p>
+
+## How to set up the project
+
+- Clone this respository to your computer
+- Access to the project folder on the computer via Termaial or Git Bash
+- Excute `npm install` to download all necessary packages
+- Excute `npm start` to start the project
+
+# pos-system
---
+++ src/fakeData/products.js
@@ -0,0 +1,50 @@
+export const products = [
+    {
+        "id": 123,
+        "name": "Soya Bin Oil",
+        "price": "200",
+        "image": "https://shodagor-uploads.s3.ap-south-1.amazonaws.com/uploads/all/lNfcu.jpg"
+    },
+    {
+        "id": 131,
+        "name": "Milk",
+        "price": "100",
+        "image": "https://cdn.pixabay.com/photo/2018/03/16/16/42/milk-3231772_960_720.jpg"
+    },
+    {
+        "id": 161,
+        "name": "Meat (Cow)",
+        "price": "800",
+        "image": "https://cdn.britannica.com/72/143572-050-87DF1262/pork-butcher-shop-Hong-Kong.jpg"
+    },
+    {
+        "id": 132,
+        "name": "Ice Cream",
+        "price": "40",
+        "image": "https://cdn.pixabay.com/photo/2016/12/26/16/09/bowl-1932375_960_720.jpg"
+    },
+    {
+        "id": 133,
+        "name": "Salmon",
+        "price": "10",
+        "image": "https://cdn.pixabay.com/photo/2021/01/05/23/18/salmon-5892659_960_720.jpg"
+    },
+    {
+        "id": 193,
+        "name": "EGG",
+        "price": "110",
+        "image": "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg"
+    },
+    {
+        "id": 134,
+        "name": "Watermelon",
+        "price": "20",
+        "image": "https://cdn.pixabay.com/photo/2015/09/27/18/18/watermelons-961128_960_720.jpg"
+    },
+    {
+        "id": 135,
+        "name": "Potato",
+        "price": "40",
+        "image": "https://cdn.pixabay.com/photo/2016/08/11/08/43/potatoes-1585060_960_720.jpg"
+    }
+]
